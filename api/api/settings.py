74141e42c8715faf47ae3fbbcadd149861eb43dc
MarcelNazare <realmarcelnazare@gmail.com>
2023-04-30 19:56:29 -0400

    added mongodb functionality

--- api/api/settings.py
+++ api/api/settings.py
@@ -72,14 +72,25 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'admin',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'mongodb+srv://realmarcelnazare:<password>@cluster0.tenofax.mongodb.net/?retryWrites=true&w=majority'
+            }
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
